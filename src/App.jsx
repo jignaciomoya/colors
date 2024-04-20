6843556c89f21800a00c9c0ec55e9b88e6e824a5
jignaciomoya <joseignaciomoyajim@gmail.com>
2024-04-20 09:48:02 +0200

    entrega

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import './App.css'
+import MyFormChallenge from './components/MyFormChallenge.jsx';
 
 function App() {
   return (
     <>
+    <MyFormChallenge />
     </>
   )
 }
--- src/components/BoxColor.jsx
+++ src/components/BoxColor.jsx
@@ -1,6 +1,29 @@
+import React, {useState, useRef, useEffect} from "react";
+
+
 function boxColor ({color, value}) {
+
+  const [classList, setClassList] = useState('');
+  const boxRef =useRef(null);
+
+  const boxStyle = {
+    backgroundColor: classList.value == `box ${value}` ? color: null,    
+    border: `2px solid ${color}`,
+    color: color
+  
+  }
+  useEffect (() => {
+    setClassList(boxRef.current.classList)
+  }, []);
   return (
     <>
+      <div 
+        ref={boxRef}
+        className={`box ${color}`}
+        style={boxStyle}>
+          {color === value ? <h3 style={{color: 'black'}}>Yes, I´m {value} color</h3> : <h3>I´m not a {value} color</h3>}
+
+      </div>
     </> 
   )
 }
